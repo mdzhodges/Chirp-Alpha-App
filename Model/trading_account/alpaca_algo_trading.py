f691ff0f81671f39af7a003ef95c02da9a6b8049
Henry Rothenberg <hsr205@aol.com>
2026-05-03 14:29:46 -0400

    Add alpaca_algo_trading.py module

---
+++ Model/trading_account/alpaca_algo_trading.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+
+import dotenv
+
+from Model.utils.constants import Constants
+
+
+class AlpacaAlgoTrading:
+
+    def __init__(self) -> None:
+        self._env_path: Path = Path(__file__).resolve().parent.parent / "Model" / ".env.example"
+        dotenv.load_dotenv(dotenv_path=self._env_path)
+
+    def get_alpaca_algo_trading_credentials_dict(self) -> dict[str, tuple[str, str]]:
+        alpaca_bullish_api_key: str = os.getenv(Constants.ALPACA_BULLISH_API_KEY, "")
+        alpaca_bullish_api_key_secret: str = os.getenv(Constants.ALPACA_BULLISH_API_KEY_SECRET, "")
+
+        alpaca_balanced_api_key: str = os.getenv(Constants.ALPACA_BALANCED_API_KEY, "")
+        alpaca_balanced_api_key_secret: str = os.getenv(Constants.ALPACA_BALANCED_API_KEY_SECRET, "")
+
+        alpaca_bearish_api_key: str = os.getenv(Constants.ALPACA_BEARISH_API_KEY, "")
+        alpaca_bearish_api_key_secret: str = os.getenv(Constants.ALPACA_BEARISH_API_KEY_SECRET, "")
+
+        alpaca_algo_trading_strategy_dict: dict[str, tuple[str, str]] = {
+            Constants.BULLISH_STR: (alpaca_bullish_api_key, alpaca_bullish_api_key_secret),
+            Constants.BALANCED_STR: (alpaca_balanced_api_key, alpaca_balanced_api_key_secret),
+            Constants.BEARISH_STR: (alpaca_bearish_api_key, alpaca_bearish_api_key_secret),
+        }
+
+        return alpaca_algo_trading_strategy_dict
